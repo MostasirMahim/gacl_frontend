c4f37bb83f65650dd916c13c1fceaae932333140
Mostasir Mahim <mahimlive@gmail.com>
2025-10-13 17:03:26 +0600

    style(choices): updated style and responsiveness.

--- src/app/(dashboard)/choices/page.tsx
+++ src/app/(dashboard)/choices/page.tsx
@@ -151,7 +151,7 @@ export default function ManageChoicesPage() {
   };
   if (isLoading) return <LoadingDots />;
   return (
-    <div className="min-h-screen bg-background ">
+    <div className="min-h-screen">
       <div className="mx-auto max-w-7xl">
         <div className="mb-8 ">
           <h1 className="text-3xl font-bold tracking-tight">
